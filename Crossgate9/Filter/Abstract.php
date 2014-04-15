20c1aa50f7b7d13e088f13a4ff6fde91a4fe025a
crossgate9 <crossgate9@gmail.com>
2014-04-15 15:22:29 +0800

    [add] pagination for filter

--- Crossgate9/Filter/Abstract.php
+++ Crossgate9/Filter/Abstract.php
@@ -3,7 +3,8 @@
 abstract class Crossgate9_Filter_Abstract {
     protected $_collection = null;
     protected $_conditions = array();
-
+    protected $_pageSize = null;
+    
     public function addCondition($_field, $_condition) {
         $this->_conditions[$_field][] = $_condition;
     }
@@ -12,12 +13,21 @@ abstract class Crossgate9_Filter_Abstract {
         $this->_collection = $_collection;
     }
 
-    public function getCollection() {
+    public function setPageSize($_pageSize) {
+        if ($_pageSize === 0) return;
+        $this->_pageSize = $_pageSize;
+    }
+
+    public function getCollection($_curPage = 1) {
         $_collection = $this->_collection;
         foreach ($this->_conditions as $_field => $_conditions) {
             foreach ($_conditions as $_condition) {
                 $_collection->addAttributeToFilter($_field, $_condition);
             }
+        }
+        if ($this->_pageSize !== null) {
+            $_collection->setPageSize($this->_pageSize);
+            $_collection->setCurPage($_curPage);
         }
         return $_collection;
     }
